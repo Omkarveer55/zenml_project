f58a71635e92d3d6c9afd7e538147402e55511a4
Omkarveer55 <omkarkawade97@gmail.com>
2023-10-24 11:30:24 +0530

    added tracking_uri

--- run_pipelines.py
+++ run_pipelines.py
@@ -1,5 +1,10 @@
 from pipelines import training_pipeline
+from zenml.client import Client
+import mlflow
 
 
 if __name__ == '__main__':
+    tracking_uri = Client().active_stack.experiment_tracker.get_tracking_uri()
+    mlflow.set_tracking_uri(tracking_uri)
     training_pipeline.train_pipeline(r'C:\Users\DELL3\zenml_pipelines\data\iris.csv')
+    
